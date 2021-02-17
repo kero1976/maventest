59391a1b01c6f613a21f0a40ce3dd15a57c2c980
kero <kero1976@outlook.jp>
2021-02-17 20:24:47 +0900

    a

---
+++ ModuleB/src/main/java/kero/moduleb/Sample1.java
@@ -0,0 +1,15 @@
+package kero.moduleb;
+
+public class Sample1 {
+	public void bug() {
+		String str = null;
+		String str2;
+		System.out.println(str);
+		try {
+			System.out.println(str.toString());
+		}catch(Exception e) {
+
+		}
+
+	}
+}
